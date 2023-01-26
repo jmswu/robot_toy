f6a49dabdc204115cf07666d82909443ae882d10
James Wu <jameswu62@yahoo.co.nz>
2023-01-26 21:06:10 +1300

    added direction()

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,7 +1,37 @@
+struct Direction
+{
+  Direction(const int pin):pin(pin)
+  {
+    pinMode(pin, OUTPUT);
+    off();
+  }
+
+  void on()
+  {
+    digitalWrite(pin, 1);
+  }
+
+  void off()
+  {
+    digitalWrite(pin, 0);
+  }
+  
+  private:
+    const int pin;
+};
+
+constexpr int FORWARD = A3;
+constexpr int LEFT = A2;
+constexpr int RIGHT = A1;
+constexpr int REVERSE = A0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  reverse.on();
+  delay(1000);
+  reverse.off();
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
